efbe016347e34e101954b7060a67553f798c2276
qwerty019 <113905629+qwerty019@users.noreply.github.com>
2025-04-30 14:28:30 +0900

    use tailwind for header and footer

--- vite-project/src/components/Footer.jsx
+++ vite-project/src/components/Footer.jsx
@@ -1,3 +1,42 @@
+// import React, { PureComponent } from 'react'
+
+// class Footer extends PureComponent {
+//     constructor(props) {
+//         super(props)
+
+//         this.state = {
+            
+//         }
+//     }
+
+//     render() {
+//         const footerStyle = {
+//             position: 'fixed',
+//             backgroundColor: 'rgba(89, 89, 89, 1)',
+//             height: '10%',
+//             width: '100%',
+//             display: 'flex',
+//             alignItems: 'center',
+//             padding: '0 20px',
+//             bottom: 0,
+//             color: 'white'
+//         };
+//         const linkStyle = {
+//             marginLeft: '20px', // Отступ между ссылками
+//             marginRight: 'auto' // Сдвигаем ссылки влево
+            
+//         };
+
+//         return (
+            
+//             <div style={footerStyle}>
+//             В случае нарушения авторских прав или других уведомлений - обращайтесь на почту liza.stepanova@mail.ru
+//             </div>
+//         )
+//     }
+// }
+
+// export default Footer
 import React, { PureComponent } from 'react'
 
 class Footer extends PureComponent {
@@ -10,29 +49,19 @@ class Footer extends PureComponent {
     }
 
     render() {
-        const footerStyle = {
-            position: 'fixed',
-            backgroundColor: 'rgba(89, 89, 89, 1)',
-            height: '10%',
-            width: '100%',
-            display: 'flex',
-            alignItems: 'center',
-            padding: '0 20px',
-            bottom: 0,
-            color: 'white'
-        };
-        const linkStyle = {
-            marginLeft: '20px', // Отступ между ссылками
-            marginRight: 'auto' // Сдвигаем ссылки влево
-            
-        };
-
-        return (
-            
-            <div style={footerStyle}>
-            В случае нарушения авторских прав или других уведомлений - обращайтесь на почту liza.stepanova@mail.ru
-            </div>
-        )
+      return (
+          <>
+          <div className='fixed bottom-0 left-0 w-full bg-gray-700'>
+              <div className="container mx-auto text-center">
+              <h2 className="text-white text-lg font-semibold">Ваш список Аниме.</h2>
+                <p className="mt-2">
+                    
+                </p>
+                <p className="mt-4 text-white">&copy; {new Date().getFullYear()} В случае нарушения авторских прав или других уведомлений - обращайтесь на почту liza.stepanova@mail.ru</p>
+              </div>
+          </div>
+          </>
+      );
     }
 }
 
--- vite-project/src/components/Header.jsx
+++ vite-project/src/components/Header.jsx
@@ -9,39 +9,53 @@ class Header extends PureComponent {
         }
     }
 
-    render() {
-        const headerStyle = {
-            position: 'fixed',
-            height: '10%',
-            width: '100%',
-            display: 'flex',
-            alignItems: 'center', // Центрирование по вертикали
-            //justifyContent: 'center',
-            padding: '0 20px' // Отступы по бокам
-        };
-        const linkStyle = {
-            marginLeft: '70px', // Отступ между ссылками
-            marginRight: 'auto', // Сдвигаем ссылки влево
-            color: 'red',
-            fontSize: '20px'
-        };
-        const linkLogoStyle = {
-            marginLeft: '70px', // Отступ между ссылками
-            marginRight: 'auto', // Сдвигаем ссылки влево
-            color: 'red',
-            fontSize: '30px'
-        };
+    // render() {
+    //     const headerStyle = {
+    //         position: 'fixed',
+    //         height: '10%',
+    //         width: '100%',
+    //         display: 'flex',
+    //         alignItems: 'center', // Центрирование по вертикали
+    //         //justifyContent: 'center',
+    //         padding: '0 20px' // Отступы по бокам
+    //     };
+    //     const linkStyle = {
+    //         marginLeft: '70px', // Отступ между ссылками
+    //         marginRight: 'auto', // Сдвигаем ссылки влево
+    //         color: 'red',
+    //         fontSize: '20px'
+    //     };
+    //     const linkLogoStyle = {
+    //         marginLeft: '70px', // Отступ между ссылками
+    //         marginRight: 'auto', // Сдвигаем ссылки влево
+    //         color: 'red',
+    //         fontSize: '30px'
+    //     };
 
-        return (
+    //     return (
             
-            <div style={headerStyle}>
-            <div style={{ flexGrow: 1 }} />
-            <a href='/' style={linkLogoStyle}>AnimeLogo</a>
-            <div style={{ flexGrow: 3 }} />
-            <a href='/news' style={linkStyle}>Новости</a>
-            <a href='/anime' style={linkStyle}>Аниме</a>
-            <a href='/list' style={linkStyle}>Списки</a>
-            <div style={{ flexGrow: 1 }} />
+    //         <div style={headerStyle}>
+    //         <div style={{ flexGrow: 1 }} />
+    //         <a href='/' style={linkLogoStyle}>AnimeLogo</a>
+    //         <div style={{ flexGrow: 3 }} />
+    //         <a href='/news' style={linkStyle}>Новости</a>
+    //         <a href='/anime' style={linkStyle}>Аниме</a>
+    //         <a href='/list' style={linkStyle}>Списки</a>
+    //         <div style={{ flexGrow: 1 }} />
+    //         </div>
+    //     )
+    // }
+
+    render() {
+        return (
+            <div className="fixed h-[10%] w-full flex items-center px-5">
+                <div className="flex-grow" />
+                <a href="/" className="ml-[70px] text-red-500 text-3xl">AnimeLogo</a>
+                <div className="flex-grow-[3]" />
+                <a href="/news" className="ml-[70px] text-red-500 text-xl">Новости</a>
+                <a href="/anime" className="ml-[70px] text-red-500 text-xl">Аниме</a>
+                <a href="/list" className="ml-[70px] text-red-500 text-xl">Списки</a>
+                <div className="flex-grow" />
             </div>
         )
     }

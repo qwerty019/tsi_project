d858d4507503bfa8c5e0361bb90202c9bc6b714c
qwerty019 <113905629+qwerty019@users.noreply.github.com>
2025-04-30 17:26:22 +0900

    Hooks (in Anime)

--- vite-project/src/pages/Anime.tsx
+++ vite-project/src/pages/Anime.tsx
@@ -1,4 +1,5 @@
-import React, { PureComponent, ReactNode } from 'react'
+import React, { PureComponent, ReactNode, useState } from 'react'
+import {Container} from '../components/Container'
 
 interface Props {}
 interface State {}
@@ -13,10 +14,100 @@ class Anime extends PureComponent<Props, State> {
     }
 
     render(): ReactNode {
-        return (
-        <div>Hello</div> 
-        )
+        
+            return <Counter/>;
+        
     }
 }
 
 export default Anime
+
+interface Animee {
+    title: string;
+    description: string;
+    price: string;
+  }
+
+const AnimeeList = ({animes}) => {
+    return (
+        <div>
+          <h2>Список аниме: </h2>
+          <ul>
+            {animes.map((anime, index) => (
+              <li key={index}>
+                <h3>{anime.title}</h3>
+                <p>{anime.description}</p>
+                <p>Оценка: {anime.price} .</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      );
+}
+
+const Counter = () => {
+    // const [count, setCount] = useState<number>(0);
+    //const [name, setName] = useState("");
+    // const [isVisible, setIsVisible] = useState(true);
+    //const [items, setItems] = useState([""]);
+    //const [newElement, setNewElement] = useState("");
+    // const [user, setUser] = useState({name:"Иван", age:30});
+    const [animes, setGoods] = useState<Animee[]>([]);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [newAnime, setNewGood] = useState<Animee>({title: '', description: '', price: ''});
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setNewGood({ ...newAnime, [name]: value });
+    };
+
+    const handleAddGood = () => {
+        if (newAnime.title && newAnime.description && newAnime.price) {
+            setGoods([...animes, newAnime]);
+            setNewGood({ title: '', description: '', price: '' });
+            setIsModalOpen(false);
+          } else {
+            alert("Пожалуйста, заполните все поля");
+          }
+    };
+
+    return (
+        <Container>
+            <div className="flex felx-col gap-4 items-center justify-center"/>
+                <div>
+                <button onClick={() => setIsModalOpen(true)}>Добавить товар</button>
+                
+                {isModalOpen && (
+                    <div className="modal">
+                    <h2>Добавить новый товар</h2>
+                    <input
+                        type="text"
+                        name="title"
+                        placeholder="Название"
+                        value={newAnime.title}
+                        onChange={handleInputChange}
+                    />
+                    <input
+                        type="text"
+                        name="description"
+                        placeholder="Описание"
+                        value={newAnime.description}
+                        onChange={handleInputChange}
+                    />
+                    <input
+                        type="number"
+                        name="price"
+                        placeholder="Оценка"
+                        value={newAnime.price}
+                        onChange={handleInputChange}
+                    />
+                    <button onClick={handleAddGood}>Добавить</button>
+                    <button onClick={() => setIsModalOpen(false)}>Закрыть</button>
+                    </div>
+                )}
+
+                <AnimeeList animes={animes} />
+            </div>
+        </Container>
+    )
+}
